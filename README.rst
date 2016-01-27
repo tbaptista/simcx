783c2f7de5d539d0162beca6155993861007a450
Tiago Baptista <baptista@dei.uc.pt>
2016-01-27 11:36:50 +0000

    Change license to Apache 2.0. Prepare the package to be released.

--- README.rst
+++ README.rst
@@ -1,11 +1,11 @@
 SimCX -- Simulator for Complex Systems
-===================================================
+======================================
 
 Simulator for Complex Systems. Developed for the course in Complex Systems from
 the Department of Informatics Engineering of the University of Coimbra.
 
-| Copyright (c) 2015 Tiago Baptista - baptista@dei.uc.pt
-| All rights reserved.
+| Copyright (c) 2015-2016 Tiago Baptista - baptista@dei.uc.pt
+| Licensed under the Apache License, Version 2.0 (see LICENSE.txt)
 
 Dependencies
 ------------
--- setup.py
+++ setup.py
@@ -1,8 +1,19 @@
 #!/usr/bin/env python
-#-----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 # Copyright (c) 2015 Tiago Baptista
-# All rights reserved.
-#-----------------------------------------------------------------------------
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# -----------------------------------------------------------------------------
 
 from setuptools import setup
 from io import open
@@ -22,7 +33,24 @@ setup(
     url='none',
     author='Tiago Baptista',
     author_email='baptista@dei.uc.pt',
+    license='Apache License 2.0',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research'
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Education',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Scientific/Engineering :: Artificial Life',
+
+
+    ],
     packages=['simcx'],
     install_requires = ['pyglet', 'matplotlib', 'numpy', 'scipy'],
-    license='LICENSE.txt'
+
 )
